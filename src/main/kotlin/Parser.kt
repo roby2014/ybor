793481ed1cd6fcc3e2e4176f90e7e0dffc35dac2
awyxx <robyawyx@gmail.com>
2022-09-27 17:31:05 +0100

    added Parser documentation and error handling

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -6,15 +6,21 @@ class Parser(private val tokens: List<Token>) {
     /** current token index */
     private var current = 0
 
-    fun parse(): Expression? {
-        return try {
-            expression()
-        } catch (e: Exception) {
-            println(e.message)
-            null
-        }
+    private class ParserError : Exception()
+
+    /** Parses tokens, returning the AST if success, null otherwise. */
+    fun parse() = try {
+        expression()
+    } catch (e: ParserError) {
+        //synchronize()
+        null
     }
 
+    /** This is where the top-down parser starts.
+     * "Recursive descent is considered a top-down parser because it starts from the
+     * top or outermost grammar rule (here expression) and works its way down
+     * into the nested subexpressions before finally reaching the leaves of the syntax tree."
+     */
     private fun expression() = equality()
 
     private fun equality(): Expression {
@@ -76,11 +82,13 @@ class Parser(private val tokens: List<Token>) {
                 consume(TokenType.RIGHT_PAREN, "Expected ')'")
                 Expression.Grouping(expr)
             }
+
             match(TokenType.NUMBER, TokenType.STRING) -> Expression.Literal(previous().literal)
-            else -> throw Exception("Expected expression")
+            else -> throw parserError(peek(), "Expected valid expression")
         }
     }
 
+    /** Returns true if one of [expected] tokens is found, advancing one. */
     private fun match(vararg expected: TokenType): Boolean {
         for (type in expected) {
             if (check(type)) {
@@ -91,24 +99,54 @@ class Parser(private val tokens: List<Token>) {
         return false
     }
 
+    /** Returns true if current token type is [expected]. */
     private fun check(expected: TokenType): Boolean = when {
         end() || peek().type != expected -> false
         else -> true
     }
 
+    /** Returns previous token. */
     private fun previous() = tokens[current - 1]
 
+    /** Returns current token. */
     private fun peek() = tokens[current]
 
+    /** Returns true if at end of list. */
     private fun end() = peek().type == TokenType.EOF
 
+    /** Advances token, returning it. */
     private fun advance(): Token {
         if (!end()) current++
         return previous()
     }
 
+    /**
+     * Consumes token by its type ([type]), advancing one.
+     * In case token did not match, throws error with message [errMsg].
+     */
     private fun consume(type: TokenType, errMsg: String): Token {
         if (check(type)) return advance()
-        throw Exception(errMsg)
+        throw parserError(peek(), errMsg)
+    }
+
+    /** Report parser error. */
+    private fun parserError(token: Token, message: String): ParserError {
+        Ybor.error(token, message)
+        return ParserError()
+    }
+
+    /**
+     * Synchronize parser after an error occurred.
+     * "Before it can get back to parsing, it needs to get its state and the sequence of
+     * forthcoming tokens aligned such that the next token does match the rule being
+     * parsed. This process is called synchronization."
+     */
+    private fun synchronize() {
+        advance()
+        while (!end()) {
+            if (previous().type == TokenType.SEMICOLON)
+                return
+            advance()
+        }
     }
 }
